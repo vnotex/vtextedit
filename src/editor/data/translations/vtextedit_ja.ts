44439e585959c2168e39f59df59652e6bc158cff
Hiroshi Miura <miurahr@linux.com>
2021-05-10 08:29:26 +0900

    Add Japanese translation (#3)
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ src/editor/data/translations/vtextedit_ja.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>vte::StatusIndicator</name>
+    <message>
+        <source>Line: %1 - %2(%3%)   Col: %4</source>
+        <translation type="vanished">行: %1 - %2(%3%)   列: %4</translation>
+    </message>
+    <message>
+        <location filename="../../texteditor/statusindicator.cpp" line="62"/>
+        <source>Spelling</source>
+        <translation>スペル</translation>
+    </message>
+    <message>
+        <location filename="../../texteditor/statusindicator.cpp" line="82"/>
+        <source>Line: %1 - %2 (%3%)   Col: %4</source>
+        <translation>行: %1 - %2 (%3%)   列: %4</translation>
+    </message>
+    <message>
+        <location filename="../../texteditor/statusindicator.cpp" line="159"/>
+        <source>Enable Spell Check</source>
+        <translation>スペルチェックを有効化</translation>
+    </message>
+    <message>
+        <location filename="../../texteditor/statusindicator.cpp" line="171"/>
+        <source>Auto Detect Language</source>
+        <translation>言語を自動検出</translation>
+    </message>
+    <message>
+        <location filename="../../texteditor/statusindicator.cpp" line="185"/>
+        <source>No Dictionary Found</source>
+        <translation>辞書が見つかりません</translation>
+    </message>
+</context>
+<context>
+    <name>vte::VTextEditTranslate</name>
+    <message>
+        <location filename="../../include/vtextedit/global.h" line="38"/>
+        <source>Insert</source>
+        <translation>挿入</translation>
+    </message>
+    <message>
+        <location filename="../../include/vtextedit/global.h" line="41"/>
+        <source>Overwrite</source>
+        <translation>上書き</translation>
+    </message>
+    <message>
+        <location filename="../../include/vtextedit/global.h" line="44"/>
+        <source>Normal (Vi)</source>
+        <translation>通常 (Vi)</translation>
+    </message>
+    <message>
+        <location filename="../../include/vtextedit/global.h" line="47"/>
+        <source>Insert (Vi)</source>
+        <translation>挿入 (Vi)</translation>
+    </message>
+    <message>
+        <location filename="../../include/vtextedit/global.h" line="50"/>
+        <source>Visual (Vi)</source>
+        <translation>ビジュアル (Vi)</translation>
+    </message>
+    <message>
+        <location filename="../../include/vtextedit/global.h" line="53"/>
+        <source>Visual Line (Vi)</source>
+        <translation>ビジュアル行 (Vi)</translation>
+    </message>
+    <message>
+        <location filename="../../include/vtextedit/global.h" line="56"/>
+        <source>Visual Block (Vi)</source>
+        <translation>ビジュアルブロック (Vi)</translation>
+    </message>
+    <message>
+        <location filename="../../include/vtextedit/global.h" line="59"/>
+        <source>Replace (Vi)</source>
+        <translation>置換 (Vi)</translation>
+    </message>
+    <message>
+        <location filename="../../include/vtextedit/global.h" line="62"/>
+        <source>Unknown</source>
+        <translation>不明</translation>
+    </message>
+    <message>
+        <location filename="../../inputmode/viinputmode.cpp" line="324"/>
+        <source>Recording</source>
+        <comment>KateVi</comment>
+        <translation>記録中</translation>
+    </message>
+    <message>
+        <location filename="../../inputmode/viinputmodefactory.cpp" line="35"/>
+        <source>Vi input mode</source>
+        <translation>Vi 入力モード</translation>
+    </message>
+    <message>
+        <location filename="../../inputmode/normalinputmodefactory.cpp" line="24"/>
+        <source>Normal input mode</source>
+        <translation>通常入力モード</translation>
+    </message>
+</context>
+<context>
+    <name>vte::VTextEditor</name>
+    <message>
+        <location filename="../../texteditor/vtexteditor.cpp" line="1254"/>
+        <source>Spelling &quot;%1&quot;</source>
+        <translation>スペル &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <location filename="../../texteditor/vtexteditor.cpp" line="1256"/>
+        <source>Ignore Word</source>
+        <translation>無視する単語</translation>
+    </message>
+    <message>
+        <location filename="../../texteditor/vtexteditor.cpp" line="1264"/>
+        <source>Add To Dictionary</source>
+        <translation>辞書へ追加</translation>
+    </message>
+    <message>
+        <location filename="../../texteditor/vtexteditor.cpp" line="1276"/>
+        <source>No Suggestions</source>
+        <translation>候補なし</translation>
+    </message>
+</context>
+</TS>
