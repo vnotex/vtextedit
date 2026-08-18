dcf02387ed3837242d94a24350e55c8bdf299798
Le Tan <tamlokveer@gmail.com>
2026-08-19 02:47:33 +0800

    fix(i18n): refresh the translation catalogs and add an lupdate target
    The table preview menu strings were wrapped in tr() but never extracted, so the zh_CN and ja catalogs had no vte::TablePreviewSheet context at all. The build only ran lrelease, leaving the .ts files to drift by hand.
    
    Add an explicitly-invoked update_translations target driven by the lupdate imported executable, refresh both catalogs (21 -> 39 messages) and translate the 14 table menu strings plus Go to Line, Line number, VSCode input mode and Zero-length match.

--- src/data/translations/vtextedit_ja.ts
+++ src/data/translations/vtextedit_ja.ts
@@ -2,120 +2,212 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ja_JP">
 <context>
-    <name>vte::StatusIndicator</name>
+    <name>QWidget</name>
     <message>
-        <source>Line: %1 - %2(%3%)   Col: %4</source>
-        <translation type="vanished">行: %1 - %2(%3%)   列: %4</translation>
+        <location filename="../../inputmode/vscodeinputmode.cpp" line="+193"/>
+        <source>Go to Line</source>
+        <translation>行へ移動</translation>
     </message>
     <message>
-        <location filename="../../texteditor/statusindicator.cpp" line="62"/>
+        <location line="+1"/>
+        <source>Line number (1-%1):</source>
+        <translation>行番号 (1-%1):</translation>
+    </message>
+</context>
+<context>
+    <name>vte::StatusIndicator</name>
+    <message>
+        <location filename="../../texteditor/statusindicator.cpp" line="+56"/>
         <source>Spelling</source>
         <translation>スペル</translation>
     </message>
     <message>
-        <location filename="../../texteditor/statusindicator.cpp" line="82"/>
+        <location line="+19"/>
         <source>Line: %1 - %2 (%3%)   Col: %4</source>
         <translation>行: %1 - %2 (%3%)   列: %4</translation>
     </message>
     <message>
-        <location filename="../../texteditor/statusindicator.cpp" line="159"/>
+        <location line="+69"/>
         <source>Enable Spell Check</source>
         <translation>スペルチェックを有効化</translation>
     </message>
     <message>
-        <location filename="../../texteditor/statusindicator.cpp" line="171"/>
+        <location line="+11"/>
         <source>Auto Detect Language</source>
         <translation>言語を自動検出</translation>
     </message>
     <message>
-        <location filename="../../texteditor/statusindicator.cpp" line="185"/>
+        <location line="+13"/>
         <source>No Dictionary Found</source>
         <translation>辞書が見つかりません</translation>
     </message>
 </context>
 <context>
+    <name>vte::TablePreviewSheet</name>
+    <message>
+        <location filename="../../markdowneditor/tablepreviewwidget.cpp" line="+1663"/>
+        <source>Table</source>
+        <translation>テーブル</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Insert Row Above</source>
+        <translation>上に行を挿入</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Insert Row Below</source>
+        <translation>下に行を挿入</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Delete Row</source>
+        <translation>行を削除</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Insert Column Left</source>
+        <translation>左に列を挿入</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Insert Column Right</source>
+        <translation>右に列を挿入</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Delete Column</source>
+        <translation>列を削除</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Alignment</source>
+        <translation>配置</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Default</source>
+        <translation>デフォルト</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Left</source>
+        <translation>左寄せ</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Center</source>
+        <translation>中央寄せ</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Right</source>
+        <translation>右寄せ</translation>
+    </message>
+    <message>
+        <location line="+34"/>
+        <source>Copy as Markdown</source>
+        <translation>Markdown としてコピー</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Copy as HTML</source>
+        <translation>HTML としてコピー</translation>
+    </message>
+</context>
+<context>
     <name>vte::VTextEditTranslate</name>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="38"/>
+        <location filename="../../include/vtextedit/global.h" line="+32"/>
         <source>Insert</source>
         <translation>挿入</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="41"/>
+        <location line="+3"/>
         <source>Overwrite</source>
         <translation>上書き</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="44"/>
+        <location line="+3"/>
         <source>Normal (Vi)</source>
         <translation>通常 (Vi)</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="47"/>
+        <location line="+3"/>
         <source>Insert (Vi)</source>
         <translation>挿入 (Vi)</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="50"/>
+        <location line="+3"/>
         <source>Visual (Vi)</source>
         <translation>ビジュアル (Vi)</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="53"/>
+        <location line="+3"/>
         <source>Visual Line (Vi)</source>
         <translation>ビジュアル行 (Vi)</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="56"/>
+        <location line="+3"/>
         <source>Visual Block (Vi)</source>
         <translation>ビジュアルブロック (Vi)</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="59"/>
+        <location line="+3"/>
         <source>Replace (Vi)</source>
         <translation>置換 (Vi)</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="62"/>
+        <location line="+3"/>
         <source>Unknown</source>
         <translation>不明</translation>
     </message>
     <message>
-        <location filename="../../inputmode/viinputmode.cpp" line="324"/>
+        <location filename="../../inputmode/viinputmode.cpp" line="+257"/>
         <source>Recording</source>
         <comment>KateVi</comment>
         <translation>記録中</translation>
     </message>
     <message>
-        <location filename="../../inputmode/viinputmodefactory.cpp" line="35"/>
+        <location filename="../../inputmode/viinputmodefactory.cpp" line="+23"/>
         <source>Vi input mode</source>
         <translation>Vi 入力モード</translation>
     </message>
     <message>
-        <location filename="../../inputmode/normalinputmodefactory.cpp" line="24"/>
+        <location filename="../../inputmode/normalinputmodefactory.cpp" line="+18"/>
         <source>Normal input mode</source>
         <translation>通常入力モード</translation>
+    </message>
+    <message>
+        <location filename="../../inputmode/vscodeinputmodefactory.cpp" line="+18"/>
+        <source>VSCode input mode</source>
+        <translation>VSCode 入力モード</translation>
     </message>
 </context>
 <context>
     <name>vte::VTextEditor</name>
     <message>
-        <location filename="../../texteditor/vtexteditor.cpp" line="1254"/>
+        <location filename="../../texteditor/vtexteditor.cpp" line="+1324"/>
+        <source>Zero-length match</source>
+        <translation>長さ 0 の一致</translation>
+    </message>
+    <message>
+        <location line="+105"/>
         <source>Spelling &quot;%1&quot;</source>
         <translation>スペル &quot;%1&quot;</translation>
     </message>
     <message>
-        <location filename="../../texteditor/vtexteditor.cpp" line="1256"/>
+        <location line="+2"/>
         <source>Ignore Word</source>
         <translation>無視する単語</translation>
     </message>
     <message>
-        <location filename="../../texteditor/vtexteditor.cpp" line="1264"/>
+        <location line="+6"/>
         <source>Add To Dictionary</source>
         <translation>辞書へ追加</translation>
     </message>
     <message>
-        <location filename="../../texteditor/vtexteditor.cpp" line="1276"/>
+        <location line="+11"/>
         <source>No Suggestions</source>
         <translation>候補なし</translation>
     </message>
--- src/data/translations/vtextedit_zh_CN.ts
+++ src/data/translations/vtextedit_zh_CN.ts
@@ -2,120 +2,212 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
-    <name>vte::StatusIndicator</name>
+    <name>QWidget</name>
     <message>
-        <source>Line: %1 - %2(%3%)   Col: %4</source>
-        <translation type="vanished">行: %1 - %2(%3%)   列: %4</translation>
+        <location filename="../../inputmode/vscodeinputmode.cpp" line="+193"/>
+        <source>Go to Line</source>
+        <translation>跳转到行</translation>
     </message>
     <message>
-        <location filename="../../texteditor/statusindicator.cpp" line="62"/>
+        <location line="+1"/>
+        <source>Line number (1-%1):</source>
+        <translation>行号 (1-%1)：</translation>
+    </message>
+</context>
+<context>
+    <name>vte::StatusIndicator</name>
+    <message>
+        <location filename="../../texteditor/statusindicator.cpp" line="+56"/>
         <source>Spelling</source>
         <translation>拼写</translation>
     </message>
     <message>
-        <location filename="../../texteditor/statusindicator.cpp" line="82"/>
+        <location line="+19"/>
         <source>Line: %1 - %2 (%3%)   Col: %4</source>
         <translation>行: %1 - %2 (%3%)   列: %4</translation>
     </message>
     <message>
-        <location filename="../../texteditor/statusindicator.cpp" line="159"/>
+        <location line="+69"/>
         <source>Enable Spell Check</source>
         <translation>启用拼写检查</translation>
     </message>
     <message>
-        <location filename="../../texteditor/statusindicator.cpp" line="171"/>
+        <location line="+11"/>
         <source>Auto Detect Language</source>
         <translation>自动检测语言</translation>
     </message>
     <message>
-        <location filename="../../texteditor/statusindicator.cpp" line="185"/>
+        <location line="+13"/>
         <source>No Dictionary Found</source>
         <translation>无可用词典</translation>
     </message>
 </context>
 <context>
+    <name>vte::TablePreviewSheet</name>
+    <message>
+        <location filename="../../markdowneditor/tablepreviewwidget.cpp" line="+1663"/>
+        <source>Table</source>
+        <translation>表格</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Insert Row Above</source>
+        <translation>在上方插入行</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Insert Row Below</source>
+        <translation>在下方插入行</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Delete Row</source>
+        <translation>删除行</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Insert Column Left</source>
+        <translation>在左侧插入列</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Insert Column Right</source>
+        <translation>在右侧插入列</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Delete Column</source>
+        <translation>删除列</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Alignment</source>
+        <translation>对齐</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Default</source>
+        <translation>默认</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Left</source>
+        <translation>左对齐</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Center</source>
+        <translation>居中</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Right</source>
+        <translation>右对齐</translation>
+    </message>
+    <message>
+        <location line="+34"/>
+        <source>Copy as Markdown</source>
+        <translation>复制为 Markdown</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Copy as HTML</source>
+        <translation>复制为 HTML</translation>
+    </message>
+</context>
+<context>
     <name>vte::VTextEditTranslate</name>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="38"/>
+        <location filename="../../include/vtextedit/global.h" line="+32"/>
         <source>Insert</source>
         <translation>插入</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="41"/>
+        <location line="+3"/>
         <source>Overwrite</source>
         <translation>改写</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="44"/>
+        <location line="+3"/>
         <source>Normal (Vi)</source>
         <translation>普通 (Vi)</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="47"/>
+        <location line="+3"/>
         <source>Insert (Vi)</source>
         <translation>插入 (Vi)</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="50"/>
+        <location line="+3"/>
         <source>Visual (Vi)</source>
         <translation>可视 (Vi)</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="53"/>
+        <location line="+3"/>
         <source>Visual Line (Vi)</source>
         <translation>可视行 (Vi)</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="56"/>
+        <location line="+3"/>
         <source>Visual Block (Vi)</source>
         <translation>可视块 (Vi)</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="59"/>
+        <location line="+3"/>
         <source>Replace (Vi)</source>
         <translation>替换 (Vi)</translation>
     </message>
     <message>
-        <location filename="../../include/vtextedit/global.h" line="62"/>
+        <location line="+3"/>
         <source>Unknown</source>
         <translation>未知</translation>
     </message>
     <message>
-        <location filename="../../inputmode/viinputmode.cpp" line="324"/>
+        <location filename="../../inputmode/viinputmode.cpp" line="+257"/>
         <source>Recording</source>
         <comment>KateVi</comment>
         <translation>记录中</translation>
     </message>
     <message>
-        <location filename="../../inputmode/viinputmodefactory.cpp" line="35"/>
+        <location filename="../../inputmode/viinputmodefactory.cpp" line="+23"/>
         <source>Vi input mode</source>
         <translation>Vi 输入模式</translation>
     </message>
     <message>
-        <location filename="../../inputmode/normalinputmodefactory.cpp" line="24"/>
+        <location filename="../../inputmode/normalinputmodefactory.cpp" line="+18"/>
         <source>Normal input mode</source>
         <translation>普通输入模式</translation>
+    </message>
+    <message>
+        <location filename="../../inputmode/vscodeinputmodefactory.cpp" line="+18"/>
+        <source>VSCode input mode</source>
+        <translation>VSCode 输入模式</translation>
     </message>
 </context>
 <context>
     <name>vte::VTextEditor</name>
     <message>
-        <location filename="../../texteditor/vtexteditor.cpp" line="1255"/>
+        <location filename="../../texteditor/vtexteditor.cpp" line="+1324"/>
+        <source>Zero-length match</source>
+        <translation>零长度匹配</translation>
+    </message>
+    <message>
+        <location line="+105"/>
         <source>Spelling &quot;%1&quot;</source>
         <translation>拼写 &quot;%1&quot;</translation>
     </message>
     <message>
-        <location filename="../../texteditor/vtexteditor.cpp" line="1257"/>
+        <location line="+2"/>
         <source>Ignore Word</source>
         <translation>忽略词</translation>
     </message>
     <message>
-        <location filename="../../texteditor/vtexteditor.cpp" line="1265"/>
+        <location line="+6"/>
         <source>Add To Dictionary</source>
         <translation>添加到词典</translation>
     </message>
     <message>
-        <location filename="../../texteditor/vtexteditor.cpp" line="1277"/>
+        <location line="+11"/>
         <source>No Suggestions</source>
         <translation>无建议</translation>
     </message>
